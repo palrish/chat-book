f01a3c53fa018242dd72903bf8cdbe91762976bb
SteezyRish <SteezyRish>
2024-06-27 15:32:41 +0530

    ready to deploy

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -24,8 +24,9 @@ io.on("connection", (socket) => {
   console.log("a user connected", socket.id);
 
   const userId = socket.handshake.query.userId;
-   console.log(userId);
   if(userId != "undefined") userSocketMap[userId] = socket.id;
+
+  console.log(",mm,da",userSocketMap);
 
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
